c4a5475d11f1a170e8dc756f6671810298d41283
Andrew Ralston <aralston+github@gmail.com>
2014-04-04 14:00:03 -0500

    facebook async load to leverage app id

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('fbPagesUiApp', [
+var app = angular.module('fbPagesUiApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
@@ -9,3 +9,15 @@ angular.module('fbPagesUiApp', [
   'fbPagesUiApp.directives',
   'ui.bootstrap'
 ]);
+
+app.run(['fbPagesApi', function (fbPagesApi) {
+  var config = fbPagesApi.config.get(function () {
+    (function(d, s, id, appId) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=" + appId;
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk', config.appId));
+  });
+}]);
