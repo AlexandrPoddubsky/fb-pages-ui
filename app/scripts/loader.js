5a5ecd6b42a72e31901709e2bc1cfc47d62658e3
Andrew Ralston <aralston@gmail.com>
2014-12-26 16:03:24 -0600

    only load fb if on big enough screen

--- app/scripts/loader.js
+++ app/scripts/loader.js
@@ -7,21 +7,23 @@ var loader = angular.module('fbPagesUiLoader', []);
 //load facebook with configured appId
 loader.run([  '$log', 'appApi',
   function ($log,      appApi) {
-    try {
-      var config = appApi.config.get(function () {
-        (function(d, s, id, appId) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {
-            return;
-          }
-          js = d.createElement(s);
-          js.id = id;
-          js.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=' + appId;
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk', config.facebook.appId));
-      });
-    } catch (e) {
-      $log.warn('Facebook loaded, but not properly...');
+    if (window.innerWidth >= 480) {
+      try {
+        var config = appApi.config.get(function () {
+          (function(d, s, id, appId) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+              return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=' + appId;
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk', config.facebook.appId));
+        });
+      } catch (e) {
+        $log.warn('Facebook loaded, but not properly...');
+      }
     }
   }]);
 
